2f94ffb372a7e613ec687a8e6ba8367d3c558ae2
milicaoui <vumilicaamm@gmail.com>
2022-05-05 02:29:49 +0200

    fix: transaction string fixed

--- connection_service/infrastructure/persistence/cypher_queries.go
+++ connection_service/infrastructure/persistence/cypher_queries.go
@@ -19,8 +19,8 @@ func checkIfConnectionExists(session neo4j.Session, user1 string, user2 string, 
 	ret, err := session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
 		var exists bool
 
-		result, err := tx.Run("MATCH (user: User {userId: $user1})-[rel:$connection]->(userConnection:User{userId: $user2}) RETURN rel",
-			map[string]interface{}{"user1": user1, "user2": user2, "connection": connection})
+		result, err := tx.Run("MATCH (user: User {userId: $user1})-[rel:"+connection+"]->(userConnection:User{userId: $user2}) RETURN rel",
+			map[string]interface{}{"user1": user1, "user2": user2})
 		if err != nil {
 			return nil, err
 		}
