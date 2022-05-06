9de93b8af23c7d37d01470b2ea081fd1afdf1b87
milicaoui <vumilicaamm@gmail.com>
2022-05-06 03:31:06 +0200

    fix: deleted duplicated function

--- api_gateway/infrastructure/services/services.go
+++ api_gateway/infrastructure/services/services.go
@@ -34,14 +34,6 @@ func NewConnectionClient(address string) connection_service.ConnectionsServiceCl
 	return connection_service.NewConnectionsServiceClient(connection)
 }
 
-func NewPostClient(address string) post_service.PostServiceClient {
-	connection, err := getClientConnection(address)
-	if err != nil {
-		log.Fatalf("Failed to start gRPC connection to Catalogue service: %v", err)
-	}
-	return post_service.NewPostServiceClient(connection)
-}
-
 func getClientConnection(address string) (*grpc.ClientConn, error) {
 	return grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 }
