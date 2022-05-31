e3e9bfef69cbf5dee3db9e8e0cca2434bf56ef50
milicaoui <vumilicaamm@gmail.com>
2022-05-31 18:52:07 +0200

    fix: neo4j driver changed

--- connection_service/infrastructure/persistence/client.go
+++ connection_service/infrastructure/persistence/client.go
@@ -2,18 +2,17 @@ package persistence
 
 import (
 	"fmt"
-
-	"github.com/neo4j/neo4j-go-driver/neo4j"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
 //vraca sesiju
 
 func GetClient(username, password, uri string) (*neo4j.Session, error) {
 
-	driver, _ := neo4j.NewDriver(uri, neo4j.BasicAuth(username, password, ""), func(c *neo4j.Config) { c.Encrypted = false })
+	driver, err := neo4j.NewDriver(uri, neo4j.BasicAuth(username, password, ""))
 	//defer driver.Close()
 
-	session, err := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
 	//defer session.Close()
 
 	if err == nil {
